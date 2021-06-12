5d7f5edeed9c81b852a0eb717281bc8a481ef931
Adam Prime <adam.prime@wattpad.com>
2021-06-12 12:51:16 -0400

    use more stuff from prebid utils, support config
    - change some variable names

--- modules/bidViewabilityIO.js
+++ modules/bidViewabilityIO.js
@@ -1,15 +1,18 @@
-
+import { config } from '../src/config.js';
 import * as events from '../src/events.js';
 import { EVENTS } from '../src/constants.json';
+import { logMessage } from '../src/utils.js';
 
-const DEBUG = true;
+const MODULE_NAME = 'bidViewabilityIO';
+const CONFIG_ENABLED = 'enabled';
 const IAB_VIEWABLE_TIME = 1000;
 const IAB_VIEWABLE_THRESHOLD = 0.5
-let SUPPORTS_IO = false;
+
+let CLIENT_SUPPORTS_IO = false;
 
 if (window.IntersectionObserver && window.IntersectionObserverEntry && window.IntersectionObserverEntry.prototype &&
     'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
-  SUPPORTS_IO = true;
+  CLIENT_SUPPORTS_IO = true;
 }
 // options for the iO that fires when the ad is viewed.  This one has no root margin, and a threshold of 1,
 // because we only want it to fire when the entire ad placement has come into view.  This might actually be too
@@ -28,7 +31,7 @@ let markViewed = (bid, entry, observer) => {
     observer.unobserve(entry.target);
     events.emit(EVENTS.BID_VIEWABLE, bid);
     entry.target.style.outline = 'green solid 5px';
-    DEBUG && console.log(entry.target.getAttribute('id') + ' was viewed');// eslint-disable-line no-console
+    logMessage(entry.target.getAttribute('id') + ' was viewed'); // TODO: should use something from bid here rather than the id probably
   }
 }
 
@@ -36,18 +39,21 @@ let markViewed = (bid, entry, observer) => {
 // when an ad comes into view, it sets a timeout for a function to be executed
 // when that ad would be considered viewed per the IAB specs. the bid that was rendered
 // is passed into the factory, so it can pass it into markViewed, so that it can be included
-// in the BID_VIEWABLE event data
+// in the BID_VIEWABLE event data. If the ad leaves view before the timer goes off, the setTimeout
+// is cancelled, an the bid will not be marked as viewable. There's probably some kind of race-ish
+// thing going on between IO and setTimeout but the browser will do what it does.
 let viewCallbackFactory = (bid) => {
   return (entries, observer) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        DEBUG && console.log('start watching view time');// eslint-disable-line no-console
+        logMessage('start watching view time'); // TODO: watching bid time on what?
         entry.target.style.outline = 'yellow solid 5px';
         entry.target.view_tracker = setTimeout(markViewed(bid, entry, observer), IAB_VIEWABLE_TIME);
       } else {
-        DEBUG && console.log(entry.target.getAttribute('id') + ' is not viewable');// eslint-disable-line no-console
+        logMessage(entry.target.getAttribute('id') + ' is out of view'); // TODO: use bid here too probably
         if (entry.target.view_tracker) {
           clearTimeout(entry.target.view_tracker);
+          logMessage(entry.target.getAttribute('id') + ' had its viewability timer stopped'); // TODO: use bid here too probably
         }
       }
     });
@@ -56,14 +62,14 @@ let viewCallbackFactory = (bid) => {
 
 export let init = () => {
   events.on(EVENTS.AD_RENDER_SUCCEEDED, ({doc, bid, id}) => {
-    console.log('oh hi there'); // eslint-disable-line no-console
-    console.log(bid); // eslint-disable-line no-console
-
-    if (SUPPORTS_IO) {
+    // read the config for the module
+    const globalModuleConfig = config.getConfig(MODULE_NAME) || {};
+    // do nothing if module-config.enabled is not set to true
+    // this way we are adding a way for bidders to know (using pbjs.getConfig('bidViewability').enabled === true) whether this module is added in build and is enabled
+    if (globalModuleConfig[CONFIG_ENABLED] && CLIENT_SUPPORTS_IO) {
       let viewable = new IntersectionObserver(viewCallbackFactory(bid), viewableOptions);
       let element = document.getElementById(bid.adUnitCode);
       viewable.observe(element);
-      element.style.outline = 'red solid 5px';
     }
   });
 }
